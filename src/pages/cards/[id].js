5307a6ef66a863ae53562164c30ba0f9aa3f7ca6
parthmern <parthmern@gmail.com>
2024-05-04 07:27:11 -0400

    file adding

--- src/pages/cards/[id].js
+++ src/pages/cards/[id].js
@@ -1,9 +1,18 @@
+
 import { useRouter } from 'next/router'
 
 //TODO: use id to query from localstorage and get correct flashcard data
 
 export default function Card() {
 	const router = useRouter()
-	console.log(router.query.id)
-	return <p>Card: {router.query.id}</p>
+	console.log(router.query.id);
+	return (
+		<div>
+
+			<p className='text-white'>Card: {router.query.id}</p>
+			
+			
+
+		</div>
+	)
 }
