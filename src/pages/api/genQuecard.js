22f8b3ae35b6696e557a329429fbbf90e74ad5e2
FadedBronze <TheSamyatGautam@gmail.com>
2024-05-04 08:06:15 -0400

    stuff and stuff

---
+++ src/pages/api/genQuecard.js
@@ -0,0 +1,50 @@
+// const apiKey = process.env.API_KEY
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const apiKey = process.env.api;
+const z = require("zod")
+
+export default async function genQuecard(req, res) {
+	console.log(req.body)
+
+	let resdata = []
+	let attempts = 1
+
+	while (attempts > 0) {
+		console.log("ran")
+		
+		const geminiResponse = sendReqestGemenai(req.body);
+		
+		try {
+			resdata = z.object({
+				question: z.string(),
+				answer: z.string(),
+			}).array().parse(JSON.parse(await geminiResponse))
+			
+			break;
+		} catch (err) {
+			attempts--;	
+		} 
+	}
+
+	if (resdata.length === 0) {
+		return res.status(500).json({ quecards: [], message: "internal server error" })
+	}
+
+	res.status(200).json({ quecards: resdata, message: "success" });
+}
+
+const sendReqestGemenai = async (info) => {
+	const genAI = new GoogleGenerativeAI(apiKey);
+	const prompt = `Give me 5 quecards in a key value pair format [
+	{
+		question: "question",
+		answer: "answer",
+	}   
+	] for the following topic ${info.value}`;
+	const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+	const result = await model.generateContent(prompt);
+	const response = await result.response;
+	const text = response.text();
+	console.log(text)
+	return text
+};
