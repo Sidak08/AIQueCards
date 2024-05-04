265caf3fdf7701e26947c46bd8a710e3a6da8ca4
parthmern <parthmern@gmail.com>
2024-05-04 11:01:59 -0400

    card effect done

--- src/pages/cards/[id].js
+++ src/pages/cards/[id].js
@@ -1,5 +1,13 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react';
+import { GridBackgroundDemo } from "@/components/ui/GridBackgroundDemo";
+import Spotlight from "@/components/ui/spotlight";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export default function Card() {
   const router = useRouter();
@@ -7,34 +15,40 @@ export default function Card() {
 
   const data = [
     {
-      "question": "What causes global warming?",
-      "answer": "CO2 emissions"
+      question: "What causes global warming?",
+      answer: "CO2 emissions",
     },
     {
-      "question": "What is the main cause of climate change?",
-      "answer": "Human activity"
+      question: "What is the main cause of climate change?",
+      answer: "Human activity",
     },
     {
-      "question": "What are the effects of climate change?",
-      "answer": "Rising sea levels, more extreme weather events, and changes in plant and animal life"
+      question: "What are the effects of climate change?",
+      answer:
+        "Rising sea levels, more extreme weather events, and changes in plant and animal life",
     },
     {
-      "question": "What can be done to address climate change?",
-      "answer": "Reduce carbon emissions, invest in renewable energy, and adapt to the effects of climate change"
+      question: "What can be done to address climate change?",
+      answer:
+        "Reduce carbon emissions, invest in renewable energy, and adapt to the effects of climate change",
     },
     {
-      "question": "What are the benefits of addressing climate change?",
-      "answer": "Improved air quality, reduced health risks, and economic benefits"
-    }
+      question: "What are the benefits of addressing climate change?",
+      answer:
+        "Improved air quality, reduced health risks, and economic benefits",
+    },
   ];
 
   const [pointerPosition, setPointerPosition] = useState({ x: 0, y: 0 });
-  const [visibleAnswers, setVisibleAnswers] = useState(Array(data.length).fill(false)); // Initialize an array to track the visibility of answers
+  const [visibleAnswers, setVisibleAnswers] = useState(
+    Array(data.length).fill(false)
+  ); // Initialize an array to track the visibility of answers
 
   useEffect(() => {
     const syncPointer = (event) => {
       const { clientX, clientY } = event;
       setPointerPosition({ x: clientX, y: clientY });
+      console.log({ x: clientX, y: clientY });
       document.documentElement.style.setProperty("--x", clientX.toFixed(2));
       document.documentElement.style.setProperty("--y", clientY.toFixed(2));
     };
@@ -52,28 +66,43 @@ export default function Card() {
     setVisibleAnswers(newVisibleAnswers);
   };
 
-
-
   return (
-    <div className='text-white'>
+    <div className="text-white relative">
+      <GridBackgroundDemo />
 
-      <p className='text-white '>Card: {router.query.id}</p>
+      <Spotlight
+        className="absolute z-[100] -top-40 left-0 md:left-1/2 transform -translate-x-1/2 md:-top-20"
+        fill="white"
+      />
 
-      <p className="text-4xl font-bold  text-center mb-8">Your Flash Cards : </p>
+      <p className="text-white ">Card: {router.query.id}</p>
 
-      <div className="flex mainCard w-full">
-        {data.map((card, index) => (
-          <article key={index} className="w-[400px] h-[300px] " data-glow>
-            <div data-glow></div>
-            <div onClick={() => toggleAnswerVisibility(index)} className="flex flex-col items-center justify-between gap-y-2 p-3">
-              <p className="inter text-2xl">{card?.question}</p>
-              {visibleAnswers[index] && <p className="lexend text-[#bec2c4] opacity-75">{card?.answer}</p>}
-            </div>
-          </article>
-        ))}
+      <p className="text-4xl font-bold text-center mb-8">Your Flash Cards:</p>
+
+      <div className="flex w-[80%] h-[80vh] mx-auto p-5 mainCard ">
+        <Swiper
+          className={"flex item-center justify-center mySwiper"}
+          navigation={true}
+          modules={[Navigation]}
+        >
+          {data.map((card, index) => (
+            <SwiperSlide className={`ml-${index * 4}`}>
+              <article className={`w-[400px] h-[300px] mx-auto`} key={index} data-glow>
+                <div data-glow></div>
+                <div
+                  onClick={() => toggleAnswerVisibility(index)}
+                  className="flex flex-col items-center justify-between gap-y-2 p-3"
+                >
+                  <p className="inter text-2xl">{card?.question}</p>
+                  {visibleAnswers[index] && (
+                    <p className="lexend text-[#bec2c4] opacity-75">{card?.answer}</p>
+                  )}
+                </div>
+              </article>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-
     </div>
-  )
-
+  );
 }
