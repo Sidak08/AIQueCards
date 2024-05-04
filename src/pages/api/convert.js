5c58e376ad87e8565ed26afee71d3a41db4ff8c5
Sidak <magicalsidak@gmail.com>
2024-05-04 06:57:13 -0400

    genQCards gemenai

--- src/pages/api/convert.js
+++ src/pages/api/convert.js
@@ -4,5 +4,5 @@ export default function convert(req, res) {
 	console.log("hi")
 	console.log(req.body)
 
-	res.status(200).json({message: "success"})	
+	res.status(200).json({message: "success"})
 }
