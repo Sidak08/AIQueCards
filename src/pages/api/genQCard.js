6a326f961b47c2844d3f7d82398cbd93c85b5859
Sidak <magicalsidak@gmail.com>
2024-05-04 07:15:57 -0400

    prasun

--- src/pages/api/genQCard.js
+++ src/pages/api/genQCard.js
@@ -8,9 +8,9 @@ export default async function convert(req, res) {
 
 const sendReqestGemenai = async (info) => {
   const genAI = new GoogleGenerativeAI(apiKey);
-  const prompt = `Give me 5 Qcards in a key value pair format {
-    qestion: "answer",
-    qestion: "answer",
+  const prompt = `Give me 5 Qcards in a key value pair format here an example format{
+    what causes global war: "CO2 emissions",
+    : "answer",
     } for the following topic ${info}`;
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const result = await model.generateContent(prompt);
