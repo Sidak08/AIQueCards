bf4f84381db0f44724c6c4079e9cc9cd9186f906
FadedBronze <TheSamyatGautam@gmail.com>
2024-05-04 15:09:22 -0400

    buggy bug sidak fixy

--- src/pages/api/convert.js
+++ src/pages/api/convert.js
@@ -1,37 +1,31 @@
 const speech = require("@google-cloud/speech");
-const fs = require('fs')
 
-export default async function convert(req, res) {
-	try {	
-		fs.writeFileSync("../../../audio/lecture.wav", req.body)
-	} catch (err) {
-		console.error(err)
-		return res.status(500).json({ message: "internal server error" })
-	}	
+export default async function convert(req, res) {	
+	const client = new speech.SpeechClient();
 
+	const config = {
+	  encoding: "mp3",
+	  sampleRateHertz: 16000,
+	  languageCode: 'en-US',
+	};
 
-	//  const gcsUri = "";
-//
-//  const audio = {
-//    uri: gcsUri,
-//  };
-//  const config = {
-//    encoding: "LINEAR16",
-//    sampleRateHertz: 16000,
-//    languageCode: "en-US",
-//  };
-//  const request = {
-//    audio: audio,
-//    config: config,
-//  };
-//
-//  const [response] = await client.recognize(request);
-//  const transcription = response.results
-//    .map((result) => result.alternatives[0].transcript)
-//    .join("\n");
-//  console.log(`Transcription: ${transcription}`);
+	const audio = {
+	  content: req.body.toString('base64'),
+	};
 
-	res.status(200).json({ message: "success" })
+	const request = {
+	  config: config,
+	  audio: audio,
+	};
+
+	// Detects speech in the audio file
+	const [response] = await client.recognize(request);
+	
+	const transcription = response.results
+  .map(result => result.alternatives[0].transcript)
+  .join('\n');
+
+	res.status(200).json({ message: "success", transcription })
 }
 
 export const config = {
--- src/pages/testsendfile.js
+++ src/pages/testsendfile.js
@@ -1,4 +1,4 @@
-import Navbar from "@/components/common/Navbar";
+import Navbar from "@/components/common/Navbar.js";
 import { useState } from "react";
 
 
@@ -20,7 +20,7 @@ export default function TestSendFile() {
 
 			<p className="text-4xl pt-20 text-white font-bold text-center mb-8">Upload mp3 </p>
 
-			<form className="pt-20 flex items-center justify-center flex-col gap-y-4 " method="post" encType="multipart/form-data">
+			<form className="pt-20 flex items-center justify-center flex-col gap-y-4 " method="post" action="/api/convert" encType="multipart/form-data">
 				<input className="mx-auto pl-[12rem] " name="lecture" accept=".mp3" type="file"></input>
 				<button onClick={(e)=>{gettingCardsFromMp3File(e)}} className="bg-white rounded-md p-2" type="submit">Submit</button>
 			</form>	
